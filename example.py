e91da6ffc09eb2bb119127d9ef29006470fc4361
Henrique Breda <henrique.breda@outlook.com>
2022-04-10 23:41:50 +0100

    feat: added a script just for example

---
+++ example.py
@@ -0,0 +1,7 @@
+expected_words = ["please", "appreciate", "would be great"]
+if not any(word in "xxxxxx f addfasdfsdf" for word in expected_words):
+    print("no exists")
+else:
+    print("exists")
+
+print("echo")
